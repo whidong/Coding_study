f1c7957fcfaf6da0b23a61eb64a596ce46a1c3eb
whidong <walwal2008@naver.com>
2022-03-15 11:20:49 +0900

    'main'에서 커밋

--- 자료구조/2차원합계산.py
+++ 자료구조/2차원합계산.py
@@ -1,9 +1,26 @@
-n=int(input())
-while True:
-  if 1<=n<=5000:
-    True
-  else:
-    print("1이상 5000이하의 정수를 입력하세요.")
-    n=int(input())
+import time, random
 
-print(n)
+def sum(A, n):
+	pass
+	# code here
+
+random.seed()
+A=[]
+
+n=int(input())
+while 1>n or n>5000:
+  print("1이상 5000이하의 정수를 입력하세요.")
+  n=int(input())
+
+B=[A,A]
+i=1
+while i<=n:
+  import random as rd
+  A.append(rd.randint(0,100))
+  i+=1
+print(A[1],A[2])
+print(B[1][2])
+
+# 리스트 A에 랜덤 정수 값 n개 채움
+# sum 함수 호출 + 시간 측정
+# 함수의 수행시간을 출력
--- 자료구조/2차원합연습.py
+++ 자료구조/2차원합연습.py
@@ -1,23 +1,21 @@
-import time, random
-
-def sum(A, n):
-	# code here
-	pass
-	
+import random
 
 random.seed()
 A=[]
 
 n=int(input())
-
-1<=n<=5000
-
-i=1
-while i<=n:
+while 1>n or n>5000:
+  print("1이상 5000이하의 정수를 입력하세요.")
+  n=int(input())
+i,j =map(int, input().split())
+B=[i,j]
+u=1
+while u<=n:
   import random as rd
   A.append(rd.randint(0,100))
-  i+=1
-
+  u+=1
+print(A[1],A[2])
+print(B[1][2])
 
 # n = 1 이상 5000 이하 정수 값 입력
 # 리스트 A에 랜덤 정수 값 n개 채움
