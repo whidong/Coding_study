031b5bbe9adec0ed3591c9e415dc3c06d2bbfa5b
whidong <walwal2008@naver.com>
2022-03-22 03:36:58 +0900

    complete

--- 자료구조/Infix-to-Postfix.py
+++ 자료구조/Infix-to-Postfix.py
@@ -38,26 +38,46 @@ def infix_to_postfix(infix):
 		prec['-'] = 1
 		prec['*'] = 2
 		prec['/'] = 2
-		prec['^'] = 3
+		prec['^'] = 3 # 연산자 우선순위 이중에 가장느림
+		
 		for token in token_list:
 				if token == '(':
 					opstack.push(token)
-				elif token == ')':
-					while opstack.top()=='(':
+				elif token == ')':                           
+					while prec[opstack.top()]!=0:
 						outstack.append(opstack.pop())
+					opstack.pop()#설명이랑 다른부분
 				elif token in '+-/*^':
-					if prec[token]<=prec[opstack.top()]:
-						outstack.append(opstack.pop())
-						opstack.push(token)
-					elif prec[token]>prec[opstack.top()]:
-						opstack.push(token)
-					elif opstack.isEmpty():
-						opstack.push(token)
+					while True: #이부분때문에 다른 식 넣었을때 연산자 순서가 크게틀어짐 반복이아닌 나열했었음
+						if opstack.isEmpty()==True:
+							opstack.push(token)
+							break
+						elif prec[token]>prec[opstack.top()]:
+							opstack.push(token)
+							break
+						else:
+							outstack.append(opstack.pop())
 				else:# operand일 때
-					outstack.append(token)# opstack 에 남은 모든 연산자를 pop 후 outstack에 append
+					outstack.append(token)
+		while opstack.isEmpty() != True:# opstack 에 남은 모든 연산자를 pop 후 outstack에 append
+			outstack.append(opstack.pop())
 		return " ".join(outstack)
 
 	
 infix_expr = input()
 postfix_expr = infix_to_postfix(infix_expr)
 print(postfix_expr)
+	
+infix_expr = input()
+postfix_expr = infix_to_postfix(infix_expr)
+print(postfix_expr)
+
+"""
+
+위에 연산자 우선순위부분에서 크게 막혔었음 
+while반복문의 사용 그리고 반복문안에 break를 적절하게 사용해주기
+^ 를 제곱이라고 생각하는 멍청이....
+^ 는 비트논리연산자의 하나로 곱셈 나눗셈보다 더 우선순위가 낮다
+이전 코드는 git에 있을테니 참고
+
+"""
