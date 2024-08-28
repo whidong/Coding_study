12289a112f69105fbebb7c6b6cb029a72e8ae0be
KIM DONGHWI <99733260+whidong@users.noreply.github.com>
2024-08-28 16:56:36 +0900

    Delete 데이터분석/단어장만들기.py

--- 데이터분석/단어장만들기.py
+++
@@ -1,8 +0,0 @@
-
-while True:
-    a = input("영어 단어를 입력하세요: ")
-    if a == "q":
-      break
-    b = input("한국어 뜻을 입력하세요: ")
-    with open("vocabulary.txt", "w") as f:
-        f.write("{}: {}\n".format(a, b))
